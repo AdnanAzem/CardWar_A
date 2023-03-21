8956abd7606880a812af4feb10a4ca6bf229de87
AdnanAzem <adnan.azem.250@gmail.com>
2023-03-21 10:32:58 +0200

    add hpp files

---
+++ sources/game.hpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+namespace ariel{
+
+};
+
+class Game{
+    private:
+
+
+    public:
+     Game(Player p1, Player p2){}
+
+
+
+     void playTurn(){}
+     void printLastTurn(){}
+
+     void playAll(){}
+
+     void printWiner(){}
+     void printLog(){}
+     void printStats(){}
+
+};
---
+++ sources/player.hpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+namespace ariel{
+
+};
+
+class Player{
+
+    private:
+     std::string name;
+
+
+
+    public:
+     Player(string name){this->name = name;};
+     int stacksize(){}
+     int cardesTaken(){}
+
+};
